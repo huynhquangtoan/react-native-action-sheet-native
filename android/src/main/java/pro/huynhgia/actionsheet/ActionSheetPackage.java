5c1298a310b7d4d2272f15ff43677118b6e3bc1c
toanhq <toanhq@edoctor.vn>
2017-10-19 11:56:47 +0700

    init

---
+++ android/src/main/java/pro/huynhgia/actionsheet/ActionSheetPackage.java
@@ -0,0 +1,28 @@
+package pro.huynhgia.actionsheet;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by toanhuynh on 19/10/2017.
+ */
+
+public class ActionSheetPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new ActionSheetModule(reactContext));
+        return modules;
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
---
+++ index.js
@@ -0,0 +1,72 @@
+/* @flow */
+
+import React from 'react'
+import { Platform, ActionSheetIOS, NativeModules } from 'react-native'
+const AndroidActionSheet = NativeModules.AndroidActionSheet
+const isIOS = Platform.OS === 'ios'
+const ActionSheetNaive = isIOS ? ActionSheetIOS : AndroidActionSheet
+
+export default class ActionSheet extends React.Component {
+  componentDidMount() {
+    const options = this.props.options
+    if (!this.isArray(options) || options.length === 0) {
+      throw Error('Prop `options` must be an array and it must not be empty.')
+    }
+  }
+  isArray(obj) {
+    return Object.prototype.toString.call(obj) === '[object Array]'
+  }
+  show() {
+    let props = this.props
+    const {
+      title,
+      message,
+      options,
+      tintColor,
+      cancelButtonIndex,
+      destructiveButtonIndex,
+      anchor,
+    } = this.props
+    let params = { ...this.props }
+    if (isIOS)
+      ActionSheetNaive.showActionSheetWithOptions(params, props.onPress)
+    else {
+      let customTitle = {
+        title: title,
+        titleColor: '#8f8f8f',
+      }
+      let cancelBtn = {
+        btnTitle: options[cancelButtonIndex],
+        btnTitleColor: tintColor ? tintColor : '#157efb',
+      }
+
+      let optionBtns = []
+      options.map((item, index) => {
+        if (index !== cancelButtonIndex) {
+          optionBtns.push({
+            btnTitle: item,
+            btnTitleColor: tintColor
+              ? tintColor
+              : index === destructiveButtonIndex ? '#fc3d39' : '#157efb',
+          })
+        }
+      })
+      params = {
+        title: customTitle,
+        optionBtns: optionBtns,
+      }
+      console.log(cancelButtonIndex)
+
+      if (
+        typeof cancelButtonIndex !== 'undefined' &&
+        cancelButtonIndex !== null
+      ) {
+        params.cancelBtn = cancelBtn
+      }
+      ActionSheetNaive.showActionSheetWithCustomOptions(params, props.onPress)
+    }
+  }
+  render() {
+    return null
+  }
+}
